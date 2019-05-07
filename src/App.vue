e05d6839281d89c3843d7b6f0336a08f80c74543
Christian Friedow <christian@friedow.com>
2019-05-07 15:44:54 +0200

    Extracted Header to own component and added styled it

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Header />
     <router-view/>
   </div>
 </template>
+
+<script lang="ts">
+
+import { Component, Vue } from 'vue-property-decorator';
+import Header from '@/components/Header.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    Header,
+  },
+})
+export default class Home extends Vue {}
+
+</script>
 
 <style lang="less">
 #app {
@@ -15,15 +26,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,99 @@
+<template>
+  <header>
+    <div class="logo-container">
+      <router-link to="/"><span class="logo">R</span></router-link>
+    </div>
+    <nav id="nav">
+      <router-link to="/types">Types</router-link>
+      <router-link to="/resources">Resources</router-link>
+    </nav>
+    <div class="user-container">
+      <router-link to="/settings">
+        <span>Username</span>
+      </router-link>
+      <router-link to="/settings">
+        <img src="https://dummyimage.com/100/616161/616161.jpg" />
+      </router-link>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Header extends Vue {}
+
+</script>
+
+<style lang="less">
+
+header {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  & > * {
+    padding: 20px;
+  }
+
+  a {
+    text-decoration: none;
+    color: #2c3e50;
+  }
+
+  .logo-container,
+  .user-container {
+    width: 150px;
+    max-width: 100%;
+  }
+
+  .logo-container {
+    text-align: left;
+  }
+
+  .logo {
+    width: 100px;
+    max-width: 100%;
+    font-size: 40px;
+  }
+
+  nav {
+    width: 700px;
+    max-width: 100%;
+    text-align: left;
+    justify-self: center;
+
+    a {
+      padding-right: 30px;
+      font-weight: bold;
+      color: #2c3e50;
+      &.router-link-exact-active {
+        color: #42b983;
+        text-decoration: underline;
+      }
+      &:last-child {
+        padding-right: 0;
+      }
+    }
+  }
+
+  .user-container {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    span {
+      padding-right: 10px;
+    }
+
+    img {
+      width: 40px;
+      border-radius: 50%;
+    }
+  }
+}
+
+</style>
