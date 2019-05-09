2f998b042edd49b5fc5d4d42029c594477c86d1a
Christian Friedow <christian@friedow.com>
2019-05-09 14:23:26 +0200

    Added create, update and delete functions for ResourceType API

--- src/apis/rembrandt/lib/ResourceType.ts
+++ src/apis/rembrandt/lib/ResourceType.ts
@@ -1,3 +1,4 @@
+import { Serializer } from 'jsonapi-serializer';
 import ApiResource from '@/apis/ApiResource';
 import Utils from '@/utils/Utils';
 
@@ -14,16 +15,69 @@ export interface ResourceType {
   attributes: ResourceTypeAttribute[];
 }
 
-// tslint:disable-next-line: max-classes-per-file
 export class ResourceTypes extends ApiResource {
-  public static async get(id?: string): Promise<ResourceType> {
-    const resourceTypesUrl = 'http://localhost:3000/resource-types';
+  // region public static methods
+  public static resourceTypesUrl(): string {
+    return 'http://localhost:3000/resource-types';
+  }
 
+  public static resourceTypeUrl(id: string): string {
+    return `${ResourceTypes.resourceTypesUrl()}/${id}`;
+  }
+
+  public static async create(resource: ResourceType): Promise<ResourceType> {
+    const resourceTypesUrl = ResourceTypes.resourceTypesUrl();
+    return await Utils.postJsonResource(resourceTypesUrl, ResourceTypes.serializer(), resource);
+  }
+
+  public static async get(id?: string): Promise<ResourceType> {
     if (id) {
-      const resourceTypeUrl = `${resourceTypesUrl}/${id}`;
+      const resourceTypeUrl = ResourceTypes.resourceTypeUrl(id);
       return await Utils.getJsonResource(resourceTypeUrl);
     }
 
+    const resourceTypesUrl = ResourceTypes.resourceTypesUrl();
     return await Utils.getJsonResource(resourceTypesUrl);
   }
+
+  public static async update(id: string, resource: ResourceType): Promise<void> {
+    const resourceTypeUrl = ResourceTypes.resourceTypeUrl(id);
+    return await Utils.patchJsonResource(resourceTypeUrl, ResourceTypes.serializer(), resource);
+  }
+
+  public static async delete(id: string): Promise<void> {
+    const resourceTypeUrl = ResourceTypes.resourceTypeUrl(id);
+    return await Utils.deleteJsonResource(resourceTypeUrl);
+  }
+  // endregion
+
+  // region private static methods
+  private static serializer(): Serializer {
+    return new Serializer('resourceType', {
+      id: '_id',
+      attributes: [
+        'name',
+        'abstract',
+        'attributes',
+        'parentType',
+      ],
+      keyForAttribute: 'camelCase',
+    });
+  }
+  // endregion
+
+  // region public members
+  // endregion
+
+  // region private members
+  // endregion
+
+  // region constructor
+  // endregion
+
+  // region public methods
+  // endregion
+
+  // region private methods
+  // endregion
 }
