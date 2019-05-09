022e409ee62d035b3ba375b2bb7fbeb38a5291af
Christian Friedow <christian@friedow.com>
2019-05-09 14:21:41 +0200

    Added abstract create function to ApiResource

--- src/apis/ApiResource.ts
+++ src/apis/ApiResource.ts
@@ -1,13 +1,37 @@
 export default abstract class ApiResource {
-  public static async get(id?: string): Promise<any> {
+  // region public static methods
+  public static async create(resource: ApiResource): Promise<ApiResource> {
     throw Error('Function has to be implemented by inheriting class.');
   }
 
-  public static async update(id: string, resource: any): Promise<any> {
+  public static async get(id?: string): Promise<ApiResource> {
     throw Error('Function has to be implemented by inheriting class.');
   }
 
-  public static async delete(id: string): Promise<any> {
+  public static async update(id: string, resource: ApiResource): Promise<void> {
     throw Error('Function has to be implemented by inheriting class.');
   }
+
+  public static async delete(id: string): Promise<void> {
+    throw Error('Function has to be implemented by inheriting class.');
+  }
+  // endregion
+
+  // region private static methods
+  // endregion
+
+  // region public members
+  // endregion
+
+  // region private members
+  // endregion
+
+  // region constructor
+  // endregion
+
+  // region public methods
+  // endregion
+
+  // region private methods
+  // endregion
 }
