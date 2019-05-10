b67bd790aed7375c1372113e64483249fdf9fe4b
Christian Friedow <christian@friedow.com>
2019-05-10 19:45:56 +0200

    Added demo of API usage

--- src/views/Types.vue
+++ src/views/Types.vue
@@ -12,7 +12,20 @@ import { ResourceTypes } from '@/apis/rembrandt/Rembrandt';
 export default class Types extends Vue {
 
   private async mounted() {
-    const resourceTypes = await ResourceTypes.get();
+    let resourceTypes = await ResourceTypes.get();
+    await ResourceTypes.create({
+      name: 'SomeType',
+      attributes: [
+        {
+          name: 'test',
+          dataType: 'string',
+          required: false,
+        },
+      ],
+      parentType: '5cd3f9c14b2ee19449808625',
+      abstract: false,
+    });
+    resourceTypes = await ResourceTypes.get();
   }
 
 }
