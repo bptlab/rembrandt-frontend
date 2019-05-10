191df035fcc08df60242bd84ac97cbf50362b8e9
Christian Friedow <christian@friedow.com>
2019-05-11 00:18:44 +0200

    Refactored fetch util functions

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -3,80 +3,58 @@ import { Serializer, Deserializer } from 'jsonapi-serializer';
 export default class Utils {
   // region public static methods
   public static async postJsonResource(url: string, serializer: Serializer, resource: any): Promise<any> {
-    const postResponse = await fetch(url, {
+    const requestOptions = {
       method: 'POST',
-      mode: 'cors',
-      cache: 'no-cache',
-      referrer: 'no-referrer',
       headers: Utils.headers(),
       body: JSON.stringify(serializer.serialize(resource)),
-    });
-    const resourceJson = await postResponse.json();
-
-    if (postResponse.status >= 300) {
-      return new Promise((resolve, reject) => {
-        const error = resourceJson.errors[0];
-        reject(new Error(`POST request failed with ${error.status} - ${error.title}: ${error.detail}`));
-      });
-    }
-
-    const deserializer = new Deserializer({ keyForAttribute: 'camelCase' });
-    return deserializer.deserialize(resourceJson);
+    };
+    return Utils.fetchResource(url, requestOptions);
   }
 
   public static async getJsonResource(url: string): Promise<any> {
-    const getResponse = await fetch(url, {
+    const requestOptions = {
       method: 'GET',
       headers: Utils.headers(),
-    });
-    const resourceJson = await getResponse.json();
-
-    if (getResponse.status >= 300) {
-      return new Promise((resolve, reject) => {
-        const error = resourceJson.errors[0];
-        reject(new Error(`POST request failed with ${error.status} - ${error.title}: ${error.detail}`));
-      });
-    }
-
-    const deserializer = new Deserializer({ keyForAttribute: 'camelCase' });
-    return deserializer.deserialize(resourceJson);
+    };
+    return Utils.fetchResource(url, requestOptions);
   }
 
   public static async patchJsonResource(url: string, serializer: Serializer, resource: any): Promise<void> {
-    const patchResponse = await fetch(url, {
+    const requestOptions = {
       method: 'POST',
       headers: Utils.headers(),
       body: JSON.stringify(serializer.serialize(resource)),
-    });
-    const resourceJson = await patchResponse.json();
-
-    if (patchResponse.status >= 300) {
-      return new Promise((resolve, reject) => {
-        const error = resourceJson.errors[0];
-        reject(new Error(`POST request failed with ${error.status} - ${error.title}: ${error.detail}`));
-      });
-    }
+    };
+    await Utils.fetchResource(url, requestOptions);
   }
 
   public static async deleteJsonResource(url: string): Promise<void> {
-    const deleteResponse = await fetch(url, {
+    const requestOptions = {
       method: 'DELETE',
       headers: Utils.headers(),
-    });
-    const resourceJson = await deleteResponse.json();
-
-    if (deleteResponse.status >= 300) {
-      return new Promise((resolve, reject) => {
-        const error = resourceJson.errors[0];
-        reject(new Error(`POST request failed with ${error.status} - ${error.title}: ${error.detail}`));
-      });
-    }
+    };
+    await Utils.fetchResource(url, requestOptions);
   }
   // endregion
 
   // region private static methods
   private static headers(): any {
     return { 'Content-Type': 'application/vnd.api+json' };
+  }
+
+  private static async fetchResource(url: string, options: RequestInit): Promise<any> {
+    const response = await fetch(url, options);
+    const responseJson = await response.json();
+
+    if (response.status >= 300) {
+      return new Promise((resolve, reject) => {
+        const error = responseJson.errors[0];
+        reject(new Error(`POST request failed with ${error.status} - ${error.title}: ${error.detail}`));
+      });
+    }
+
+    const deserializer = new Deserializer({ keyForAttribute: 'camelCase' });
+    return deserializer.deserialize(responseJson);
   }
   // endregion
 
