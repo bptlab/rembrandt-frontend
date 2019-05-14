a0e6e91a72c6725800f3121cab55b880d819eb51
Christian Friedow <christian@friedow.com>
2019-05-14 15:44:29 +0200

    Adjusted styling to match dark mode

--- src/App.vue
+++ src/App.vue
@@ -20,11 +20,39 @@ export default class Home extends Vue {}
 </script>
 
 <style lang="less">
+@import "colors";
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+body {
+  background-color: @primary-bg;
+}
+
+main {
+  text-align: left;
+  margin: 0px 30px;
+  max-width: 900px;
+  width: 100%;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+ul,
+li {
+  margin: 0;
+  color: @primary;
 }
 </style>
