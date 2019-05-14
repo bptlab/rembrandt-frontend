c48660aebdff072cd796415e225794179ba0fa4c
Sven <sven.ladusch@web.de>
2019-05-14 18:16:16 +0200

    resource instances are visualized in a long list

--- src/views/Resources.vue
+++ src/views/Resources.vue
@@ -8,14 +8,14 @@
 
 import { Component, Vue } from 'vue-property-decorator';
 import ListSection, { ListElement } from '@/components/ListSection.vue';
-import { ResourceInstance, ResourceInstances } from '@/apis/rembrandt/rembrandt';
+import { ResourceInstance, ResourceInstances, ResourceType } from '@/apis/rembrandt/rembrandt';
 
 @Component({
   components: {
     ListSection,
   },
 })
-export default class Resources extends Vue {}
+export default class Resources extends Vue {
   // region public static methods
   // endregion
 
@@ -23,6 +23,25 @@ export default class Resources extends Vue {}
   // endregion
 
   // region public members
+  public resourceInstances: ResourceInstance[] = [];
+
+  public get resourceInstanceForType(): ResourceInstance[] {
+
+    //filter for resourcetype:
+    return this.resourceInstances;
+  }
+
+  public get resourceInstancesList(): ListElement[] {
+    //create list for specific type
+    return this.resourceInstanceForType.map((resourceInstance) => {
+      return {
+        id: `${resourceInstance._id}`,
+        //first value needs to be the identifying value
+        firstValue: `${resourceInstance._id}`,
+        secondValue: `Resource Type: ${resourceInstance.resourceType}`,
+      };
+    });
+  }
   // endregion
 
   // region private members
@@ -32,8 +51,12 @@ export default class Resources extends Vue {}
   // endregion
 
   // region public methods
+  public async mounted() {
+    this.resourceInstances = await ResourceInstances.get();
+  }
   // endregion
 
   // region private methods
   // endregion
+}
 </script>
