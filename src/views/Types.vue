ab2166176bd7ef68d45bb4e6ea269c7cbaa2a141
Christian Friedow <christian@friedow.com>
2019-05-14 15:45:21 +0200

    Rendered resource Types as a list

--- src/views/Types.vue
+++ src/views/Types.vue
@@ -1,13 +1,73 @@
 <template>
   <main>
+    <ListSection title="Abstract Types" :list="abstractResourceTypesList" />
+    <ListSection title="Non-Abstract Types" :list="nonAbstractResourceTypesList" />
   </main>
 </template>
 
 <script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { ResourceTypes, ResourceType } from "@/apis/rembrandt/rembrandt";
+import ListSection, { ListElement } from "@/components/ListSection.vue";
 
-import { Component, Vue } from 'vue-property-decorator';
+@Component({
+  components: {
+    ListSection,
+  }
+})
+export default class Types extends Vue {
+  // region public static methods
+  // endregion
 
-@Component
-export default class Types extends Vue {}
+  // region private static methods
+  // endregion
 
+  // region public members
+  public resourceTypes: ResourceType[] = [];
+
+  public get abstractResourceTypes(): ResourceType[] {
+    return this.resourceTypes.filter(resourceType => resourceType.abstract);
+  }
+
+  public get nonAbstractResourceTypes(): ResourceType[] {
+    return this.resourceTypes.filter(resourceType => !resourceType.abstract);
+  }
+
+  public get abstractResourceTypesList(): ListElement[] {
+    return this.abstractResourceTypes.map((resourceType) => {
+      return {
+        id: resourceType._id || resourceType.name,
+        firstValue: resourceType.name,
+        secondValue: `Parent Type: ${resourceType.parentType}`,
+      }
+    })
+  }
+
+  public get nonAbstractResourceTypesList(): ListElement[] {
+    return this.nonAbstractResourceTypes.map((resourceType) => {
+      return {
+        id: resourceType._id || resourceType.name,
+        firstValue: resourceType.name,
+        secondValue: `Parent Type: ${resourceType.parentType}`,
+        thirdValue: `300 Instances`,
+      }
+    })
+  }
+  // endregion
+
+  // region private members
+  // endregion
+
+  // region constructor
+  // endregion
+
+  // region public methods
+  public async mounted() {
+    this.resourceTypes = await ResourceTypes.get();
+  }
+  // endregion
+
+  // region private methods
+  // endregion
+}
 </script>
