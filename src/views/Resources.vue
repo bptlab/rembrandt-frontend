8982f1d41ea2998baf7200616bbf1438225b48f8
Sven <sven.ladusch@web.de>
2019-05-14 16:55:13 +0200

    initialized regions and imports

--- src/views/Resources.vue
+++ src/views/Resources.vue
@@ -6,8 +6,29 @@
 <script lang="ts">
 
 import { Component, Vue } from 'vue-property-decorator';
+import ListSection, { ListElement } from '@/components/ListSection.vue';
+import { ResourceInstance, ResourceInstances } from '@/apis/rembrandt/rembrandt';
 
 @Component
 export default class Resources extends Vue {}
+  // region public static methods
+  // endregion
 
+  // region private static methods
+  // endregion
+
+  // region public members
+  // endregion
+
+  // region private members
+  // endregion
+
+  // region constructor
+  // endregion
+
+  // region public methods
+  // endregion
+
+  // region private methods
+  // endregion
 </script>
