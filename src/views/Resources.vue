3dc101e4e0efe2f46fda48a2ea67f1c389352429
Sven <sven.ladusch@web.de>
2019-05-21 11:08:20 +0200

    added search bar and functionality to instance view

--- src/views/Resources.vue
+++ src/views/Resources.vue
@@ -1,6 +1,7 @@
 <template>
   <main>
     <ListSection :key="element.id" v-for="element in listOfTypesWithResources" :title="element.name" :list="resourceInstanceForType(element)" />
+    <input v-model="searchTerm" placeholder="">
   </main>
 </template>
 
@@ -26,28 +27,32 @@ export default class Resources extends Vue {
   // region public members
   public resourceInstances: ResourceInstance[] = [];
   public listOfResourceTypes: ResourceType[] = [];
+  public searchTerm: string = "";
 
   public get listOfTypesWithResources() {
+  // get all resourcetypes with instances
+  public get listOfTypesWithResources(): ResourceType[] {
     return this.listOfResourceTypes.filter((resourceType) => {
       return this.resourceInstanceForType(resourceType).length
     });
   }
 
-  public get allResourceInstances(): ListElement[] {
-    return Utils.resourceInstancesToList(this.resourceInstances);
-  }
-
+  // match resourceinstance and corresponding type and search filter
   public resourceInstanceForType(resourceType : ResourceType): ListElement[] {
-    //filter for resourcetype:
     const resourcesPerType = this.resourceInstances.filter((resourceInstance) => {
       return (resourceInstance.resourceType === resourceType.id && this.filterInstances(resourceInstance) );
     });
     return Utils.resourceInstancesToList(resourcesPerType);
   }
+  // filter instances for search term
+  public filterInstances(resourceInstance : ResourceInstance): boolean {
+    if (resourceInstance.id){
+      return (resourceInstance.resourceType.includes(this.searchTerm) ||
+            resourceInstance.id.includes(this.searchTerm));
+    } else {
+      return false;
+    }
 
-  public filterInstances(resourceInstance : ResourceInstance) {
-    return (resourceInstance.resourceType.includes("208") ||
-            resourceInstance.id.includes("208"))
   }
   // endregion
 
