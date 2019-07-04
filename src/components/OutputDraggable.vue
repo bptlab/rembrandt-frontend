7580f1aee417516e6b6490b5d5d5f6535f085fe5
Christian Friedow <christian@friedow.com>
2019-07-04 12:15:03 +0200

    Added draggable output

---
+++ src/components/OutputDraggable.vue
@@ -0,0 +1,58 @@
+<template>
+  <div ref="draggable" class="draggable" :style="{transform: translate}">
+    <div ref="inputDropzone" :class="{inputConnector: !isBeeingDragged}" />
+    <div class="element">
+      <span>Output Draggable: {{ingredientObject.name}}</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop } from 'vue-property-decorator';
+import Draggable, { DropzoneEvent } from '@/components/Draggable.vue';
+import { OutputIngredient } from '@/plugins/RecipeModeler';
+import { ResourceType } from '@/apis/rembrandt/rembrandt';
+import interact from 'interactjs';
+
+@Component
+export default class OutputDraggable extends Draggable implements OutputIngredient {
+  // region public static methods
+  // endregion
+
+  // region private static methods
+  // endregion
+
+  // region public members
+  @Prop()
+  public input?: Draggable;
+
+  @Prop({ type: Object })
+  public ingredientObject!: ResourceType;
+  // endregion
+
+  // region private members
+  // endregion
+
+  // region constructor
+  // endregion
+
+  // region public methods
+  public mounted() {
+    this.enableDropzone(this.$refs.inputDropzone as HTMLDivElement);
+  }
+
+  public dropped(event: DropzoneEvent): void {
+    if (event.detail.dropzone === this.$refs.inputDropzone) { return; }
+    this.adjustPositionToDropzone(event.detail.draggable, event.detail.dropzone);
+  }
+  // endregion
+
+  // region private methods
+  // endregion
+}
+</script>
+
+
+<style lang="less">
+@import "../colors.less";
+</style>
