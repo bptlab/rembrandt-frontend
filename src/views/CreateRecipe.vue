efc0ddd936a454f9b714a5d7c0fa78abd737b2a0
Christian Friedow <christian@friedow.com>
2019-07-11 16:16:17 +0200

    Added ingredient components to create recipe

--- src/views/CreateRecipe.vue
+++ src/views/CreateRecipe.vue
@@ -1,11 +1,62 @@
 <template>
-  <main>
-    <Draggable v-for="element in $recipeModeler.recipe" :key="element" />
-    <InputDraggable :ingredientObject="inputObject.ingredientObject" />
-    <OutputDraggable :ingredientObject="outputObject.ingredientObject" />
-    <TransformerDraggable :ingredientObject="transformerObject.ingredientObject" />
-    <AlgorithmDraggable :ingredientObject="algorithmObject.ingredientObject" />
-    <button @click="addDraggable">Add Draggable</button>
+  <main class="create-recipe">
+    <div class="ingredients">
+      <div class="add-inputs">
+        <h2>Inputs</h2>
+        <button
+          v-for="resourceType in resourceTypes"
+          :key="resourceType.id"
+          @click="addInputIngredient(resourceType)"
+        >{{ resourceType.name }}</button>
+      </div>
+      <div class="add-outputs">
+        <h2>Outputs</h2>
+        <button
+          v-for="resourceType in resourceTypes"
+          :key="resourceType.id"
+          @click="addOutputIngredient(resourceType)"
+        >{{ resourceType.name }}</button>
+      </div>
+      <div class="add-transformers">
+        <h2>Transformers</h2>
+        <button
+          v-for="transformer in transformers"
+          :key="transformer.id"
+          @click="addTransformerIngredient(transformer)"
+        >{{ transformer.name }}</button>
+      </div>
+      <div class="add-algorithms">
+        <h2>Algorithms</h2>
+        <button
+          v-for="algorithm in algorithms"
+          :key="algorithm.id"
+          @click="addAlgorithmIngredient(algorithm)"
+        >{{ algorithm.name }}</button>
+      </div>
+    </div>
+
+    <div class="playground">
+      <InputDraggable
+        v-for="ingredient in inputIngredients"
+        :key="ingredient.ingredientObject.id"
+        :ingredientObject="ingredient.ingredientObject"
+      />
+      <OutputDraggable
+        v-for="ingredient in outputIngredients"
+        :key="ingredient.ingredientObject.id"
+        :ingredientObject="ingredient.ingredientObject"
+      />
+      <TransformerDraggable
+        v-for="ingredient in transformerIngredients"
+        :key="ingredient.ingredientObject.id"
+        :ingredientObject="ingredient.ingredientObject"
+      />
+      <AlgorithmDraggable
+        v-for="ingredient in algorithmIngredients"
+        :key="ingredient.ingredientObject.id"
+        :ingredientObject="ingredient.ingredientObject"
+      />
+    </div>
   </main>
 </template>
 
@@ -17,7 +68,17 @@ import OutputDraggable from '@/components/OutputDraggable.vue';
 import TransformerDraggable from '@/components/TransformerDraggable.vue';
 import AlgorithmDraggable from '@/components/AlgorithmDraggable.vue';
 import { InputIngredient, OutputIngredient, TransformerIngredient, AlgorithmIngredient } from '@/plugins/RecipeModeler';
-import { ResourceTypeNullObject, ResourceTypes, createTransformerNullObject, Transformers, createOptimizationAlgorithmNullObject, OptimizationAlgorithms } from '@/apis/rembrandt/rembrandt';
+import {
+  ResourceTypeNullObject,
+  ResourceTypes,
+  createTransformerNullObject,
+  Transformers,
+  Transformer,
+  createOptimizationAlgorithmNullObject,
+  OptimizationAlgorithms,
+  ResourceType,
+  OptimizationAlgorithm,
+} from '@/apis/rembrandt/rembrandt';
 import interact from 'interactjs';
 
 @Component({
@@ -37,51 +98,108 @@ export default class CreateRecipe extends Vue {
   // endregion
 
   // region public members
-  public inputObject: InputIngredient = {
-    ingredientObject: ResourceTypeNullObject,
-  };
-
-  public outputObject: OutputIngredient = {
-    ingredientObject: ResourceTypeNullObject,
-  };
-
-  public transformerObject: TransformerIngredient = {
-    ingredientObject: createTransformerNullObject(),
-  };
-
-  public algorithmObject: AlgorithmIngredient = {
-    ingredientObject: createOptimizationAlgorithmNullObject(),
-  };
   // endregion
 
   // region private members
+  private inputIngredients: InputIngredient[] = [];
+  private outputIngredients: OutputIngredient[] = [];
+  private transformerIngredients: TransformerIngredient[] = [];
+  private algorithmIngredients: AlgorithmIngredient[] = [];
+
+  private resourceTypes: ResourceType[] = [];
+  private transformers: Transformer[] = [];
+  private algorithms: OptimizationAlgorithm[] = [];
   // endregion
 
   // region constructor
   // endregion
 
   // region public methods
-  public addDraggable() {
-    this.$recipeModeler.addRecipeElement();
-  }
-
   public async mounted() {
-    const resourceTypes = await ResourceTypes.get();
-    this.inputObject.ingredientObject = resourceTypes[3];
-    this.outputObject.ingredientObject = resourceTypes[1];
-    const transformers = await Transformers.get();
-    this.transformerObject.ingredientObject = transformers[0];
-    const algorithms = await OptimizationAlgorithms.get();
-    this.algorithmObject.ingredientObject = algorithms[0];
+    this.resourceTypes = await ResourceTypes.get();
+    this.transformers = await Transformers.get();
+    this.algorithms = await OptimizationAlgorithms.get();
   }
   // endregion
 
   // region private methods
+  private addInputIngredient(resourceType: ResourceType) {
+    this.inputIngredients.push({
+      inputs: [],
+      ingredientObject: resourceType,
+      position: {
+        x: 0,
+        y: 0,
+      },
+    });
+  }
+
+  private addOutputIngredient(resourceType: ResourceType) {
+    this.outputIngredients.push({
+      inputs: [],
+      ingredientObject: resourceType,
+      position: {
+        x: 0,
+        y: 0,
+      },
+    });
+  }
+
+  private addTransformerIngredient(transformer: Transformer) {
+    this.transformerIngredients.push({
+      inputs: [],
+      ingredientObject: transformer,
+      position: {
+        x: 0,
+        y: 0,
+      },
+    });
+  }
+
+  private addAlgorithmIngredient(algorithm: OptimizationAlgorithm) {
+    this.algorithmIngredients.push({
+      inputs: [],
+      ingredientObject: algorithm,
+      position: {
+        x: 0,
+        y: 0,
+      },
+    });
+  }
   // endregion
 }
 </script>
 
 <style lang="less">
 @import "../colors.less";
+
+main.create-recipe {
+  max-width: unset;
+  display: flex;
+
+  .ingredients,
+  .ingredients > * {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .ingredients {
+    margin: @spacing;
+    min-width: 200px;
+
+    & > * {
+      padding: @spacing 0;
+    }
+
+    & > *:first-child {
+      padding: 0;
+    }
+  }
+
+  .playground {
+    flex: 1 0;
+    margin: @spacing 0;
+  }
+}
 </style>
 
