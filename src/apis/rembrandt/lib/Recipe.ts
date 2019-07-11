8723f059c0db1c87ac2a2213e612070c49f9b71b
Christian Friedow <christian@friedow.com>
2019-07-11 17:00:28 +0200

    Added recipe api

---
+++ src/apis/rembrandt/lib/Recipe.ts
@@ -0,0 +1,68 @@
+import { Serializer } from 'jsonapi-serializer';
+import Resource from '@/apis/jsonapi/Resource';
+import CRUDResource from '@/apis/jsonapi/CRUDResource';
+import config from '@/config/config.json';
+import { ResourceType, OptimizationAlgorithm, Transformer } from '@/apis/rembrandt/rembrandt';
+
+export interface Ingredient {
+  inputs: Ingredient[];
+  ingredientObject: Resource;
+  position: {
+    x: number;
+    y: number;
+  };
+}
+
+export interface InputIngredient extends Ingredient {
+  ingredientObject: ResourceType;
+}
+
+export interface TransformerIngredient extends Ingredient {
+  ingredientObject: Transformer;
+}
+
+export interface AlgorithmIngredient extends Ingredient {
+  ingredientObject: OptimizationAlgorithm;
+}
+
+export interface OutputIngredient extends Ingredient {
+  ingredientObject: ResourceType;
+}
+
+
+export interface Recipe extends Resource {
+  name: string;
+  ingredients: Ingredient;
+}
+
+const serializer = new Serializer('recipes', {
+  id: 'id',
+  attributes: [
+    'name',
+    'ingredients',
+  ],
+  keyForAttribute: 'camelCase',
+});
+
+export const Recipes = new CRUDResource<Recipe>(
+  `${config.backendHost}/optimization/recipes`,
+  serializer,
+);
+
+export function createRecipeNullObject(): Recipe {
+  return {
+    name: '',
+    ingredients: createIngredientNullObject(),
+  };
+}
+
+export function createIngredientNullObject(): Ingredient {
+  return {
+    inputs: [],
+    ingredientObject: {},
+    position: {
+      x: 0,
+      y: 0,
+    },
+  };
+}
--- src/apis/rembrandt/rembrandt.ts
+++ src/apis/rembrandt/rembrandt.ts
@@ -3,4 +3,5 @@ export * from '@/apis/rembrandt/lib/ResourceType';
 export * from '@/apis/rembrandt/lib/ResourceInstance';
 export * from '@/apis/rembrandt/lib/OptimizationAlgorithm';
 export * from '@/apis/rembrandt/lib/Transformer';
+export * from '@/apis/rembrandt/lib/Recipe';
 export * from '@/apis/rembrandt/lib/transformerTypes';
