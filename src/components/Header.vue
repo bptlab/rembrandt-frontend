32ed9e66f18f04685dbc7afc3072f582251b3a00
Sven <sven.ladusch@web.de>
2019-07-14 23:26:31 +0200

    added recipes to Burgermenu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -71,6 +71,9 @@
                 <router-link :to="{ name: 'transformers' }">Transformers</router-link>
               </li>
               <li>
+                <router-link :to="{ name: 'recipes' }">Recipes</router-link>
+              </li>
+              <li>
                 <router-link :to="{ name: 'executions' }">Executions</router-link>
               </li>
             </ul>
